19c73c47f6f476529a10fd63a787470d99df8eb5
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2018-01-15 14:15:11 +0100

    Add prettier rule + add command line options

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 type Rule struct {
@@ -15,6 +16,8 @@ type Rule struct {
 const (
 	ACCESSIBILITY = "ACCESSIBILITY"
 	PERFORMANCE   = "PERFORMANCE"
+	QUALITY   = "QUALITY"
+	WEB   = "WEB"
 )
 
 func createFile(path string) {
@@ -35,18 +38,76 @@ func createFile(path string) {
 	fmt.Println("==> done creating file", path)
 }
 
+	
+func Filter(vs []Rule, f func(Rule) bool) []Rule {
+    vsf := make([]Rule, 0)
+    for _, v := range vs {
+        if f(v) {
+            vsf = append(vsf, v)
+        }
+    }
+    return vsf
+}
+
+func Contain(ruleTag string, tags []string) bool {
+	for _, tag := range tags {
+		if ruleTag == tag {
+			return true
+		}
+	}
+	return false
+}
+
+func Contains(ruleTags []string, tags []string) bool {
+	
+	for _, tag := range ruleTags {
+		if !Contain(tag, tags){
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 
-	path := os.Args[1] + "/checklist.md"
+	var pathDir string
+	var tags []string
+	
+	for i, option := range os.Args[0:] {
+		switch option {
+		case "-r": 
+		pathDir = os.Args[i+1]
+			break;
+		case "-t":
+			tags = strings.Split(os.Args[i+1], ",")
+			break
+		default: 
+			break;
+		}
+	}		
+	
+	if pathDir == "" {
+		panic("You should define the path of the generated audit report file")
+	}
+	if len(tags) == 0 {
+		panic("You should define a comma separated list of tags")
+	}
 
+	rules := []Rule{
+		{id: "lang_attribute", path: "lang_attribute.md", tags: []string{ACCESSIBILITY, WEB}},
+		{id: "prettier", path: "prettier.md", tags: []string{QUALITY, WEB}},
+	}
+
+	filteredRules := Filter(rules, func(rule Rule) bool {
+		return Contains(rule.tags, tags)
+	})
+
+	path := pathDir  + "/checklist.md"
 	createFile(path)
 	file, _ := os.OpenFile(path, os.O_RDWR, 0644)
 	defer file.Close()
 
-	rules := []Rule{
-		{id: "lang_attribute", path: "lang_attribute.md", tags: []string{ACCESSIBILITY}},
-	}
-	for _, rule := range rules {
+	for _, rule := range filteredRules {
 
 		dat, err := ioutil.ReadFile("/home/manu/Documents/workspaces/golang/src/generator/rules/" + rule.path)
 		if err != nil {
---
+++ rules/prettier.md
@@ -0,0 +1,4 @@
+# Utilisation de Prettier
+
+Afin d'uniformiser le code source d'un projet dans une équipe de développeurs, nous pouvons ajouter dans le workflow de développement, l'outil 
+Prettier, développé par Facebook. 
